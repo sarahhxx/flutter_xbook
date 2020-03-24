c404834c74e4698bcf6253bc2bae73bfe801b79e
sarahhxx <1220797472@qq.com>
2020-03-24 18:04:10 +0800

    增加底部菜单栏

--- lib/app/RootScene.dart
+++ lib/app/RootScene.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RootScene extends StatefulWidget {
@@ -8,6 +9,16 @@ class RootScene extends StatefulWidget {
 class RootSceneState extends State<RootScene> {
   int _tabIndex = 1;
   bool isFinishSetup = false;
+  List<Image> _tabImages = [
+    Image.asset('img/tab_bookshelf_n.png'),
+    Image.asset('img/tab_bookstore_n.png'),
+    Image.asset('img/tab_me_n.png'),
+  ];
+  List<Image> _tabSelectedImages = [
+    Image.asset('img/tab_bookshelf_p.png'),
+    Image.asset('img/tab_bookstore_p.png'),
+    Image.asset('img/tab_me_p.png'),
+  ];
 
   @override
   void initState() {
@@ -18,6 +29,7 @@ class RootSceneState extends State<RootScene> {
 
   setupApp() async {
     setState(() {
+      isFinishSetup = true;
     });
   }
 
@@ -25,15 +37,43 @@ class RootSceneState extends State<RootScene> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: IndexedStack(
-        children: <Widget>[
-        BookshelfScene(),
-        HomeScene(),
-        MeScene(),
-        ],
-        index: _tabIndex,
+          children: <Widget>[
+            BookshelfScene(),
+            HomeScene(),
+            MeScene(),
+          ],
+          index: _tabIndex,
         ),
+
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: Colors.white,
+        activeColor: SQColor.primary,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: getTabIcon(0), title: Text('书架')),
+          BottomNavigationBarItem(icon: getTabIcon(1), title: Text('书城')),
+          BottomNavigationBarItem(icon: getTabIcon(2), title: Text('我的')),
+        ],
+        currentIndex: _tabIndex,
+        onTap: (index){
+          setState(() {
+            _tabIndex = index;
+          });
+        },
+      ),
+
+
     );
+
+
+
   }
 
+  Image getTabIcon(int index){
+    if (index == _tabIndex) {
+      return _tabSelectedImages[index];
+    } else {
+      return _tabImages[index];
+    }
+  }
 
 }
